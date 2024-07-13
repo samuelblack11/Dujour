5d5b4c7f6158d57ea762e3b59fbfb65c12d2541d
sblack <snb1582@gmail.com>
2024-07-12 21:54:07 -0400

    adding basic yml files for admin and customer

--- dujour-desktop/adminDashboard/server/server.js
+++ dujour-desktop/adminDashboard/server/server.js
@@ -4,8 +4,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');  // Import the 'path' module here
 const app = express();
-app.use(cors());
 app.use(express.json()); // Middleware to parse JSON bodies
+
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' ? 'https://dujour-admin.azurewebsites.net' : '*',
+};
+app.use(cors(corsOptions));
+
 const port = process.env.PORT || 3000; // Change 3001 to your preferred port
 
 // URL encode the password to handle special characters
@@ -14,54 +19,51 @@ const encodedPassword = encodeURIComponent(process.env.mongoPwd);
 
 // Construct the MongoDB URI using imported configuration
 const uri = `mongodb+srv://${encodedUsername}:${encodedPassword}@${process.env.mongoClusterName}.mongodb.net/Dujour?retryWrites=true&w=majority&appName=${process.env.databaseName}`
-
-mongoose.connect(uri)
-  .then(() => {
-    console.log('Connected to MongoDB Atlas database...');
-    
-    // Ensure we're using the correct database
-    const db = mongoose.connection.db;
-    console.log('Database name:', db.databaseName);
-
-    // List collection names
-    db.listCollections().toArray((err, collections) => {
-      if (err) {
-        console.error('Error listing collections:', err);
-        return;
-      }
-      console.log('Collections:');
-      collections.forEach(collection => console.log(collection.name));
-    });
-  })
+mongoose.connect(uri, { useNewUrlParser: true})
+  .then(() => console.log('Connected to database...'))
   .catch(err => {
     console.error('Could not connect to MongoDB...', err);
-    process.exit(1); // Exit the process to avoid running the server without a database connection
+    process.exit(1);
   });
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(express.static(path.join(__dirname, 'client/public')));
+// Serve static files based on the environment
+const staticPath = process.env.NODE_ENV === 'production' ? 
+    path.join(__dirname, '../client/build') : 
+    path.join(__dirname, '../client/public');
+console.log(`Serving static files from ${staticPath}`);
+app.use(express.static(staticPath));
+
+const BASE_API_PATH = process.env.BASE_API_PATH || '/api';
+// Create a router for all API routes
+const apiRouter = express.Router();
 
 // Import and use routes
 const farmRoutes = require('dujour-shared/routes/farms');
 const itemRoutes = require('dujour-shared/routes/items');
 const orderRoutes = require('dujour-shared/routes/orders');
 const userRoutes = require('dujour-shared/routes/users');
+const deliveryRoutes= require('dujour-shared/routes/deliveryRoutes');
+const pickPlans= require('dujour-shared/routes/pickPlans');
 const optimizeDeliveries = require('dujour-shared/routes/optimize-deliveries')({
   googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
 });
 
-const deliveryRoutes= require('dujour-shared/routes/deliveryRoutes');
-const pickPlans= require('dujour-shared/routes/pickPlans');
+apiRouter.use('/farms', farmRoutes);
+apiRouter.use('/items', itemRoutes);
+apiRouter.use('/orders', orderRoutes);
+apiRouter.use('/users', userRoutes);
+apiRouter.use('/deliveryRoutes', deliveryRoutes);
+apiRouter.use('/pickPlans', pickPlans);
+apiRouter.use('/optimize-deliveries', optimizeDeliveries);
 
-app.use('/api/farms', farmRoutes);
-app.use('/api/items', itemRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/optimize-deliveries', optimizeDeliveries);
-app.use('/api/deliveryRoutes', deliveryRoutes);
-app.use('/api/pickPlans', pickPlans);
+// Then mount the API router
+app.use(BASE_API_PATH, apiRouter);
 
-
+// Catch-all handler for React app
+app.get('*', (req, res) => {
+  console.log(`Serving React app for route: ${req.path}`);
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- dujour-desktop/customerFacing/server/server.js
+++ dujour-desktop/customerFacing/server/server.js
@@ -4,8 +4,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');  // Import the 'path' module here
 const app = express();
-app.use(cors());
 app.use(express.json()); // Middleware to parse JSON bodies
+
+const corsOptions = {
+  origin: process.env.NODE_ENV === 'production' ? 'https://dujour-mobile.azurewebsites.net' : '*',
+};
+app.use(cors(corsOptions));
+
 const port = process.env.PORT || 3002; // Change 3001 to your preferred port
 
 // URL encode the password to handle special characters
@@ -15,22 +20,23 @@ const encodedPassword = encodeURIComponent(process.env.mongoPwd);
 // Construct the MongoDB URI using imported configuration
 const uri = `mongodb+srv://${encodedUsername}:${encodedPassword}@${process.env.mongoClusterName}.mongodb.net/Dujour?retryWrites=true&w=majority&appName=${process.env.databaseName}`
 
-mongoose.connect(uri)
-  .then(() => {
-    console.log('Connected to MongoDB Atlas database...');
-    
-    // Ensure we're using the correct database
-    const db = mongoose.connection.db;
-    console.log('Database name:', db.databaseName);
-  })
+mongoose.connect(uri, { useNewUrlParser: true})
+  .then(() => console.log('Connected to database...'))
   .catch(err => {
     console.error('Could not connect to MongoDB...', err);
-    process.exit(1); // Exit the process to avoid running the server without a database connection
+    process.exit(1);
   });
 
+// Serve static files based on the environment
+const staticPath = process.env.NODE_ENV === 'production' ? 
+    path.join(__dirname, '../client/build') : 
+    path.join(__dirname, '../client/public');
+console.log(`Serving static files from ${staticPath}`);
+app.use(express.static(staticPath));
 
-app.use(express.static(path.join(__dirname, 'client/build')));
-app.use(express.static(path.join(__dirname, 'client/public')));
+const BASE_API_PATH = process.env.BASE_API_PATH || '/api';
+// Create a router for all API routes
+const apiRouter = express.Router();
 
 // Import and use routes
 const farmRoutes = require('dujour-shared/routes/farms');
@@ -40,12 +46,21 @@ const userRoutes = require('dujour-shared/routes/users');
 const optimizeDeliveries= require('dujour-shared/routes/optimize-deliveries');
 const deliveryRoutes= require('dujour-shared/routes/deliveryRoutes');
 
-app.use('/api/farms', farmRoutes);
-app.use('/api/items', itemRoutes);
-app.use('/api/orders', orderRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/optimize-deliveries', optimizeDeliveries);
-app.use('/api/deliveryRoutes', deliveryRoutes);
+apiRouter.use('/farms', farmRoutes);
+apiRouter.use('/items', itemRoutes);
+apiRouter.use('/orders', orderRoutes);
+apiRouter.use('/users', userRoutes);
+apiRouter.use('/optimize-deliveries', optimizeDeliveries);
+apiRouter.use('/deliveryRoutes', deliveryRoutes);
+
+// Then mount the API router
+app.use(BASE_API_PATH, apiRouter);
+
+// Catch-all handler for React app
+app.get('*', (req, res) => {
+  console.log(`Serving React app for route: ${req.path}`);
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
